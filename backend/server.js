3cd7f613a01ba8b69d716e770979682f23c7757b
Ikenna Ndumele <ndumeleikenna@gmail.com>
2026-05-11 16:32:11 +0100

    Fix API contact route

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/contact", contactRoutes);
+app.use("/api/contact", contactRoutes);
 app.get("/", (req, res) => {
   res.send("Backend running");
 });
