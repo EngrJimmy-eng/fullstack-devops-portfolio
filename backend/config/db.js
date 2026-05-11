61ede9b0fd9381feac769e6d3160e18d1ab4ea71
Ikenna Ndumele <ndumeleikenna@gmail.com>
2026-05-11 18:26:52 +0100

    Fix DB connection pool and API stability

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,13 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: "ikenna-mysql",
-  user: "ikenna_user",
-  password: "strongpassword",
-  database: "ikenna_db"
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("DB connection failed:", err);
-  } else {
-    console.log("Connected to MySQL");
-  }
+  user: "root",
+  password: "root",
+  database: "ikenna_db",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = db;
