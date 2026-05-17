035d223b31774cab45f7362cef95e6ec6e824155
Ikenna Ndumele <ndumeleikenna@gmail.com>
2026-05-17 19:19:25 +0100

    Updated server.js with bcryptjs

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
-const db = require('./config/db');
+
+const db = require("./config/db");
 
 const mysql = require("mysql2");
 
+const bcrypt = require("bcryptjs");
+
 const express = require("express");
+
 const cors = require("cors");
 
 const contactRoutes = require("./routes/contact");
@@ -11,9 +15,11 @@ const contactRoutes = require("./routes/contact");
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 app.use("/api/contact", contactRoutes);
+
 app.get("/", (req, res) => {
   res.send("Backend running");
 });
