8c67d2f191e10598cf85c1043b5ed7e432225c3d
Ikenna Ndumele <ndumeleikenna@gmail.com>
2026-06-02 09:06:54 +0100

    Temporarily updated AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,30 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 
-import MainLayout from "../components/MainLayout";
-import Home from "../pages/Home";
-import Contact from "../pages/Contact";
-import Projects from "../pages/Projects";
-import ProjectDetail from "../pages/ProjectDetail";
-
-import Login from "../admin/Login";
-import Dashboard from "../admin/Dashboard";
-
 export default function AppRoutes() {
   return (
     <Routes>
-
-      {/* Public pages with layout */}
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/projects/:slug" element={<ProjectDetail />} />
-      </Route>
-
-      {/* Admin routes (no layout) */}
-      <Route path="/admin/login" element={<Login />} />
-      <Route path="/admin/dashboard" element={<Dashboard />} />
-
+      <Route path="/" element={<h1>HOME OK</h1>} />
     </Routes>
   );
 }
